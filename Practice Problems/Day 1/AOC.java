66ba1d4e514d074004c59658746bbd54793294fc
Yuvraj <yuvraj@gmail.com>
2025-08-11 11:17:40 +0530

    Day 1 practice problems

---
+++ Practice Problems/Day 1/AOC.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AOC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.println(Math.PI*a*a);
+    }
+}
